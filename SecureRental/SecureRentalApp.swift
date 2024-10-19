e5f4f51a0ad2992c4e5f6cf6ac5b54c30bb53ec2
Haniya Akhtar <haniyaakhtar4@gmail.com>
2024-10-19 12:54:50 -0400

    Initial Commit

---
+++ SecureRental/SecureRentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureRentalApp.swift
+//  SecureRental
+//
+//  Created by Haniya Akhtar on 2024-10-19.
+//
+
+import SwiftUI
+
+@main
+struct SecureRentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
