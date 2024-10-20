f71b20891a579b936d87762b97403e5756741529
Haniya Akhtar <haniyaakhtar4@gmail.com>
2024-10-20 17:27:09 -0400

    skeleton of home page with navigation menu and chatbot icon

---
+++ SecureRental/Views/HomeView.swift
@@ -0,0 +1,66 @@
+//
+//  HomeView.swift
+//  SecureRental
+//
+//  Created by Haniya Akhtar on 2024-10-19.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @Binding var rootView: RootView
+    @State private var selectedTab = 0
+    
+    var body: some View {
+        ZStack {
+            //main TabView Content
+            TabView(selection: $selectedTab) {
+                NavigationView {
+                    //Rental Listings
+                }
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+                .tag(0)
+                
+                Text("Favorites")
+                    .tabItem {
+                        Label("Messages", systemImage: "star")
+                    }
+                    .tag(1)
+                
+                Text("Profile")
+                    .tabItem {
+                        Label("Profile", systemImage: "person")
+                    }
+                    .tag(2)
+            }
+            
+            //chatbot icon
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        //action to show the chatbot
+                        print("Chatbot tapped")
+                    }) {
+                        Image(systemName: "bubble.right.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .clipShape(Circle())
+                            .shadow(radius: 10)
+                    }
+                    .padding(.bottom, 50)
+                    .padding(.trailing, 20)
+                }
+            }
+        }
+    }
+}
+    
