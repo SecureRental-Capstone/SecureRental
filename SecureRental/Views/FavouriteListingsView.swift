589c7f982e15468333ffdcdca967b31fc4be29de
anchal <anchlsharma32@gmail.com>
2024-11-14 22:07:39 -0500

    added facouriteListingsView (this is only basic now, it will be updated once the business logic will be implemented)

---
+++ SecureRental/Views/FavouriteListingsView.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteListingsView.swift
+//  SecureRental
+//
+//  Created by Anchal  Sharma  on 2024-11-14.
+//
+
+import SwiftUI
+
+struct FavouriteListingsView: View {
+    var body: some View {
+        Text("Favorites")
+    }
+}
+
+#Preview {
+    FavouriteListingsView()
+}
