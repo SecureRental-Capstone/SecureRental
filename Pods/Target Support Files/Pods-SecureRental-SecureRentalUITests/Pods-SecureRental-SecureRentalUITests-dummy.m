d01e4d2b1345e799473ba123a400855a52f5455a
Haniya Akhtar <haniyaakhtar4@gmail.com>
2025-09-22 21:07:26 -0400

    Removing incompatible dependencies

--- Pods/Target Support Files/Pods-SecureRental-SecureRentalUITests/Pods-SecureRental-SecureRentalUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SecureRental_SecureRentalUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SecureRental_SecureRentalUITests
-@end
--- Pods/Target Support Files/Pods-SecureRentalTests/Pods-SecureRentalTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SecureRentalTests : NSObject
-@end
-@implementation PodsDummy_Pods_SecureRentalTests
-@end
